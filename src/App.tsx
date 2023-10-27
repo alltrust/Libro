3639757164d0b61b463520305fdf41f7aa62889b
alltrust <aldorgarcia29@gmail.com>
2023-10-27 16:33:13 -0400

    transpiling jsx to es2015 via esbuild

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,49 @@
+import { ChangeEvent, useEffect, useState, useRef } from "react";
+import * as esbuild from "esbuild-wasm";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
+
+  const ref = useRef<any>();
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: "/esbuild.wasm",
+    });
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const handleInput = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setInput(e.target.value);
+  };
+
+  const handleCodeClick = async () => {
+    if (!ref.current) {
+      return;
+    }
+
+    //transpile the jsx
+    const result = await ref.current.transform(input, {
+      loader: "jsx",
+      target: "es2015",
+    });
+
+    setCode(result.code);
+  };
+
   return (
-    <>
-      <h1 className="text-2xl text-slate-500">HELLO MUNDO</h1>
-    </>
+    <div>
+      <textarea value={input} onChange={handleInput}></textarea>
+      <div>
+        <button onClick={handleCodeClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
   );
 }
 
